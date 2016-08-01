e6789f437846514be3e360d398cb8b7f416e33cc
Pierre-Henry Soria <hizup.com@hotmail.com>
2016-08-01 11:02:43 +0100

    Easier for compile it with GCC

---
+++ compile.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+#  compile.sh
+#  JoraLang
+#
+#  Created by Pierre-Henry Soria on 01/08/2016.
+#  Copyright © 2016 Pierre-Henry Soria. All rights reserved.
+
+g++ -c *.cpp
+g++ *.o -o JoraLang
