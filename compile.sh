cd92252e7794bbdb4daa154e015e3e422dbcbf8a
Pierre-Henry Soria <hi@ph7.me>
2026-09-07 17:28:27 +1000

    Make compiler helper reproducible from any directory

--- compile.sh
+++ compile.sh
@@ -6,5 +6,7 @@
 #  Created by Pierre-Henry Soria on 01/08/2016.
 #  Copyright © 2016 Pierre-Henry Soria. All rights reserved.
 
-g++ -c *.cpp
-g++ *.o -o JoraLang
+set -eu
+cd "$(dirname "$0")"
+"${CXX:-c++}" -std=c++11 -Wall -Wextra -pedantic \
+    Core.cpp Lexer.cpp Parser.cpp main.cpp -o JoraLang
